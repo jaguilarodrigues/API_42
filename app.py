36f38364891f7deda040a0615ad060159462eac7
Jaqueline R Aguilar <jaguilarodrigues@gmail.com>
2020-07-14 19:11:38 -0300

    static post student

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from resources.students import Students
+from resources.students import Students, Student
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Students, '/students')
+api.add_resource(Student, '/students/<int:id>')
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- resources/students.py
+++ resources/students.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 students = [
     {
@@ -24,3 +24,27 @@ students = [
 class Students(Resource):
 	def get(self):
 		return students
+
+class Student(Resource):
+
+	st_arg = reqparse.RequestParser()
+	st_arg.add_argument('name')
+	st_arg.add_argument('intra_id')
+	# st_arg.add_argument('projects')
+
+
+	def get(self, id):
+		for student in students:
+			if student['id'] == id:
+				return student
+		return {'messsage': 'Student Not found'}, 404
+
+	def post(self, id):
+		data = Student.st_arg.parse_args()
+
+		new_student = {'id':id, **data}
+
+		students.append(new_student)
+		return new_student, 201
+
+	
